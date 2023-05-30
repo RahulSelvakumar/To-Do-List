a7254796644690ac53c7e02c5686136f9b01648e
RahulSelvakumar <112105461+RahulSelvakumar@users.noreply.github.com>
2023-05-30 19:22:19 +0530

    Added mongodb database using mongoose

--- app.js
+++ app.js
@@ -1,28 +1,45 @@
 const express=require("express");
 const bodyParser=require("body-parser");
+const mongoose=require("mongoose");
 const app=express();
-let newTasks=[];
+
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 app.set('view engine','ejs');
 
+mongoose.connect("mongodb://127.0.0.1:27017/todolistDB");
+const listSchema= new mongoose.Schema({
+    name:String
+});
+const Item=mongoose.model("Item",listSchema);
+
 app.get("/",function(req,res){
-    let today=new Date();
-    let options={
-        weekday:'long',
-        day:'numeric',
-        month:'long',
-    };
-    let day=today.toLocaleDateString("en-US",options);
-    res.render("list",{listTitle:day, listTask:newTasks});
+    Item.find({},function(err,foundItems){
+        res.render("list",{listTitle:"Today", listTask:foundItems});
+    });
 })
 
 app.post("/",function(req,res){
-    let newTask=req.body.task;
-    newTasks.push(newTask);
+    let itemName=req.body.task;
+    const item = new Item({
+        name:itemName
+    });
+    Item.insertMany(item);
     res.redirect("/");  
 })
+
+app.post("/delete",function(req,res){
+    const deleteItem=req.body.check;  
+    Item.findByIdAndRemove({_id:deleteItem},function(err){
+        if(!err){
+            console.log("Sucess");
+        }
+    })
+    res.redirect("/");
+});
+
+
 app.listen(3000,function(){
     console.log("Listining to port 3000");
 })  
